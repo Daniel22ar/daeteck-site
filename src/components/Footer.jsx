3f3c7f1b38151cfd16eb7a4a7d7a4ab93849819e
Desingnet <daniel.arredondo@desingnet.com>
2025-06-04 23:43:33 -0600

    V.1.2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
@@ -16,30 +18,32 @@ const Footer = () => {
         <div className="footer-section links">
           <h3>Legales</h3>
           <ul>
-            <li><a href='/public/AvisoPrivacidad.pdf' target="_blank" rel="noopener noreferrer">Aviso de privacidad</a></li>
-            <li><a href='/public/CartaDerechosMinimos.pdf' target="_blank" rel="noopener noreferrer">Carta de derechos minimos</a></li>
-            <li><a href='/public/AdministracionRed.pdf' target="_blank" rel="noopener noreferrer">Codigo de politicas de gestion de trafico y administracion de red</a></li>
-            <li><a href='/public/PracticasComerciales.pdf' target="_blank" rel="noopener noreferrer">Codigo de practicas comerciales</a></li>
-            <li><a href='/public/InformacionSeguridad.pdf' target="_blank" rel="noopener noreferrer">Informacion Seguridad y Justicia Area Responsable</a></li>
-            <li><a href='/public/InformacionTransparente.pdf' target="_blank" rel="noopener noreferrer">Información Transparente Comparable</a></li>
+            <li><Link to="/AvisoPrivacidad.pdf" target="_blank" rel="noopener noreferrer">Aviso de privacidad</Link></li>
+            <li><Link to="/CartaDerechosMinimos.pdf" target="_blank" rel="noopener noreferrer">Carta de derechos minimos</Link></li>
+            <li><Link to="/AdministracionRed.pdf"target="_blank" rel="noopener noreferrer" >Código de políticas de gestión de tráfico y administración de red</Link></li>
+            <li><Link to="/PracticasComerciales.pdf"target="_blank" rel="noopener noreferrer" >Código de prácticas comerciales</Link></li>
+            <li><Link to="/InformacionSeguridad.pdf" target="_blank" rel="noopener noreferrer">Información Seguridad y Justicia Area Responsable</Link></li>
+            <li><Link to="/InformacionTransparente.pdf" target="_blank" rel="noopener noreferrer" >Información Transparente Comparable</Link></li>
           </ul>
         </div>
 
         <div className="footer-section links">
           <h3>Información</h3>
           <ul>
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/nosotros">Nosotros</a></li>
-            <li><a href="/servicios">Servicios</a></li>
-            <li><a href="/contacto">Contacto</a></li>
-            <li><a href="/cobertura">Cobertura</a></li>
-          </ul>
+            <li><NavLink className= {({isActive}) => isActive ? "active" : ""} to="/">Inicio</NavLink></li>
+            <li><NavLink className= {({isActive}) => isActive ? "active" : ""} to="/nosotros">Nosotros</NavLink></li>
+            <li><NavLink className= {({isActive}) => isActive ? "active" : ""} to="/servicios">Servicios</NavLink></li>
+            <li><NavLink className= {({isActive}) => isActive ? "active" : ""} to="/contacto">Contacto</NavLink></li>
+            <li><NavLink className= {({isActive}) => isActive ? "active" : ""} to="/cobertura">Cobertura</NavLink></li>
+            <li><NavLink className= {({isActive}) => isActive ? "active" : ""} to="/paquetes">Paquetes</NavLink></li>
+            <li><NavLink className= {({isActive}) => isActive ? "active" : ""} to="/pago-online">Pago Online</NavLink></li>
+            </ul>
         </div>
 
         <div className="footer-section contact">
           <h3>Contáctanos</h3>
-          <p>Email:contacto@daetecknetwork.com</p>
-          <p>Teléfono: +52 33 3332 4089</p> 
+          <p>Email: <a href="mailto:contacto@daetecknetwork.com">contacto@daetecknetwork.com</a></p>
+          <p>Teléfono: <a href="tel:+523333324089">+52 33 3332 4089</a></p>
           <p>Dirección: Hermanos Echaury Pte #17, Concepcion de Buenos Aires C.P. 49170 Jalisco</p> 
         </div>
       </div>
